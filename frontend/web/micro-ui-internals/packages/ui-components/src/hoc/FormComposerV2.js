3a825eb704690bc3344154158ea36fa6401bd9ab
KolaganiNavya <kolaganinavya18@gmail.com>
2025-05-27 11:55:02 +0530

    pull from STUDIO-UI-4

--- frontend/web/micro-ui-internals/packages/ui-components/src/hoc/FormComposerV2.js
+++ frontend/web/micro-ui-internals/packages/ui-components/src/hoc/FormComposerV2.js
@@ -372,11 +372,11 @@ export const FormComposer = (props) => {
       {props.submitInForm && (
         <SubmitBar label={t(props.label)} style={{ width:"100%",...props?.buttonStyle }} submit="submit" disabled={isDisabled} className="w-full"/>
       )}
-      {/* {props.secondaryActionLabel && (
+      {props.secondaryActionLabel && (
         <div className="primary-label-btn" style={{ margin: "20px auto 0 auto" }} onClick={onSecondayActionClick}>
           {props.secondaryActionLabel}
         </div>
-      )} */}
+      )}
     </React.Fragment>
   );
 
@@ -429,7 +429,7 @@ export const FormComposer = (props) => {
           )}
         </HorizontalNav>
       )}
-      {!props.submitInForm && props.label && props?.secondaryActionLabel && (
+      {!props.submitInForm && props.label && (
         <Footer>
           {props?.draftLabel && (  <SubmitBar className="digit-formcomposer-submitbar" submit="button" label={t(props?.draftLabel)} onClick={props?.onSecondayActionClick} />)}
           <SubmitBar label={t(props.label)} className="digit-formcomposer-submitbar" submit="submit" disabled={isDisabled} />
