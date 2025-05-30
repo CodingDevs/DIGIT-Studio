011c915d32ce7623a065bc6142425319e83e20f3
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-05-30 08:56:54 +0530

    added individual & service request services from the health repo
    referenced from branch https://github.com/egovernments/health-campaign-services/tree/update-feature
    
    latest commit https://github.com/egovernments/health-campaign-services/commit/d69d20e673a7f86f707772249909b0cdfdd0aba1

---
+++ core-services/individual/src/main/java/org/egov/individual/Constants.java
@@ -0,0 +1,25 @@
+package org.egov.individual;
+
+public interface Constants {
+    String NAME_BASED_SEARCH_EXCEPTION_MESSAGE = "Error during individual-name-based search: ";
+    String NAME_BASED_SEARCH_FAILED_CODE = "INDIVIDUAL_NAME_SEARCH_FAILED";
+    String SET_INDIVIDUALS = "setIndividuals";
+    String VALIDATION_ERROR = "VALIDATION_ERROR";
+    String SYSTEM_GENERATED = "SYSTEM_GENERATED";
+    String GET_INDIVIDUALS = "getIndividuals";
+    String GET_SKILLS = "getSkills";
+    String GET_IDENTIFIERS = "getIdentifiers";
+    String GET_ADDRESS = "getAddress";
+    String GET_IDENTIFIER_TYPE = "getIdentifierType";
+    String GET_TYPE = "getType";
+    String GET_ID = "getId";
+    String INDIVIDUAL_CREATE_LOCALIZATION_CODE = "INDIVIDUAL_NOTIFICATION_ON_CREATE";
+    String INDIVIDUAL_UPDATE_LOCALIZATION_CODE = "INDIVIDUAL_NOTIFICATION_ON_UPDATE";
+    String INDIVIDUAL_NOTIFICATION_ENG_LOCALE_CODE = "en_IN";
+    String INDIVIDUAL_MODULE_CODE = "rainmaker-masters";
+    String INDIVIDUAL_LOCALIZATION_CODES_JSONPATH = "$.messages.*.code";
+    String INDIVIDUAL_LOCALIZATION_MSGS_JSONPATH = "$.messages.*.message";
+    String ORG_ADMIN_ROLE_CODE = "ORG_ADMIN";
+    String INVALID_TENANT_ID = "INVALID_TENANT_ID";
+    String INVALID_TENANT_ID_MSG = "Tenant ID is not valid";
+}
