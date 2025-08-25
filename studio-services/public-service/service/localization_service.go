a1a5b878017d7572b39e37ba55245458df600aac
debasishchakraborty-egovt <debasish@egovernmets.org>
2025-08-25 19:05:12 +0530

    Added chnages to add Business Service instead of module

--- studio-services/public-service/service/localization_service.go
+++ studio-services/public-service/service/localization_service.go
@@ -598,7 +598,7 @@ func removeDuplicateCodes(messages []model.Message) []model.Message {
 func (l LocalizationService) ChecklistLocalization(data map[string]interface{}, req model.ServiceRequest) {
 	var messages []model.Message
 	locale := os.Getenv("NOTIFICATION_LOCALE")
-	module := (req.Service.Module)
+	module := (req.Service.BusinessService)
 	localizationModule := os.Getenv("LOCALIZATION_MODULE") + module
 
 	// Extract checklist data
